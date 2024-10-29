e720925a40ac67f26483c0c7209f593539963033
Edson Carvalho Inturia <30557202+edsoncarvalhointuria@users.noreply.github.com>
2024-10-28 21:51:23 -0300

    voltando noticias

--- Projeto_Furia/funcoes.py
+++ Projeto_Furia/funcoes.py
@@ -19,18 +19,17 @@ def carregar_ranking(dataframe):
 
 
 def carregar_noticias(pesquisa:str):
-    pass
-    # API_KEY = {"X-Api-Key": os.getenv("NEWS_API")}
-    # link = f"https://newsapi.org/v2/everything?q={pesquisa}&sortBy=popularity&language=pt&apiKey={API_KEY["X-Api-Key"]}"
-    # requisicao = requests.get(link)
+    API_KEY = {"X-Api-Key": os.getenv("NEWS_API")}
+    link = f"https://newsapi.org/v2/everything?q={pesquisa}&sortBy=popularity&language=pt&apiKey={API_KEY["X-Api-Key"]}"
+    requisicao = requests.get(link)
 
-    # if requisicao.ok:
-    #     requisicao_dict = requisicao.json()
-    #     for noticia in requisicao_dict["articles"]:
-    #         if "furia" in noticia.get("content").lower() or "furia" in noticia.get("description").lower() or pesquisa in noticia.get("content").lower() or pesquisa in noticia.get("description").lower() or "esports" in noticia.get("description").lower():
-    #             with st.sidebar.expander(noticia["title"], expanded=False):
-    #                 st.write(f"```{datetime.strptime(noticia["publishedAt"], "%Y-%m-%dT%H:%M:%SZ").strftime("%d/%m/%Y")}```")
-    #                 st.markdown(noticia["description"][:130]+"...", help=noticia["description"])
-    #                 st.markdown(f"[Leia mais]({noticia["url"]})", unsafe_allow_html=True)
-    # else:
-    #     st.markdown("Sem noticias no momento...")
+    if requisicao.ok:
+        requisicao_dict = requisicao.json()
+        for noticia in requisicao_dict["articles"]:
+            if "furia" in noticia.get("content").lower() or "furia" in noticia.get("description").lower() or pesquisa in noticia.get("content").lower() or pesquisa in noticia.get("description").lower() or "esports" in noticia.get("description").lower():
+                with st.sidebar.expander(noticia["title"], expanded=False):
+                    st.write(f"```{datetime.strptime(noticia["publishedAt"], "%Y-%m-%dT%H:%M:%SZ").strftime("%d/%m/%Y")}```")
+                    st.markdown(noticia["description"][:130]+"...", help=noticia["description"])
+                    st.markdown(f"[Leia mais]({noticia["url"]})", unsafe_allow_html=True)
+    else:
+        st.markdown("Sem noticias no momento...")
